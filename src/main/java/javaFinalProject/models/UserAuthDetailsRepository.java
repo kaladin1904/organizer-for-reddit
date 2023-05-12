ab6149563d4c6bc7546719cbb14c94bfd8e5672c
kaladin1904 <im2410@nyu.edu>
2023-05-12 15:06:56 -0400

    heroku changes

--- src/main/java/javaFinalProject/models/UserAuthDetailsRepository.java
+++ src/main/java/javaFinalProject/models/UserAuthDetailsRepository.java
@@ -4,6 +4,9 @@ import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 
+import jakarta.transaction.Transactional;
+
+@Transactional
 public interface UserAuthDetailsRepository extends CrudRepository<UserAuthDetails, UUID> {
     UserAuthDetails findByUsername(String username);
 }
